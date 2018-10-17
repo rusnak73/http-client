ef44d9e43357fabf362e666c30a90ed6d8c898eb
Dmytro_Rusnak <rysuy7@gmail.com>
2018-10-17 19:10:07 +0300

    Fixing layout bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,29 +18,26 @@ export class AppComponent implements OnInit {
     this.srv.getData(this.URL)
       .subscribe(data =>{ 
         let html = '';
+        document.querySelector('.list-container .row').innerHTML = '';
         for(let i = 21; i<27; i++){
           let adminStatus = data[i].site_admin ? "Yes" : "No";
           html += `
-          <div class="list-container container">
-          <div class="row justify-content-between">
-          <div class="user d-flex">
-          <div class="user-logo">
-          <img class="user_image" src="${data[i].avatar_url}" alt="Image of ${data[i].login}">
+          <div _ngcontent-c0 class="user d-flex">
+          <div _ngcontent-c0 class="user-logo">
+          <img _ngcontent-c0 class="user_image" src="${data[i].avatar_url}" alt="Image of ${data[i].login}">
           </div>
-          <div class="user-info">
-          <h2 class="user-name">${data[i].login}</h2>
-          <p class="admin-value"><strong>Site Administrator:</strong> ${adminStatus}!</p>
+          <div _ngcontent-c0 class="user-info">
+          <h2 _ngcontent-c0 class="user-name">${data[i].login}</h2>
+          <p _ngcontent-c0 class="admin-value"><strong _ngcontent-c0>Site Administrator:</strong> ${adminStatus}!</p>
           </div>
           </div>
-          <div class="user_hyperlink">
-          <a class="btn btn-primary" href="${data[i].html_url}">Profile</a>
-          </div>
-          </div>
+          <div _ngcontent-c0 class="user_hyperlink">
+          <a _ngcontent-c0 class="btn btn-primary" href="${data[i].html_url}">Profile</a>
           </div>`;
           // let userList = '';
           
       }
-      document.getElementsByClassName('user-list')[0].innerHTML+=html;
+      document.querySelector('.list-container .row').innerHTML+=html;
       
   
         console.log(data)})
